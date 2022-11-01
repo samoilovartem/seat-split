57484d12f96319249c415f524e476c39877f8b99
Artem Samoilov <samoylovartem07@gmail.com>
2022-11-01 22:30:54 +0800

    18th commit

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -156,8 +156,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 # USE IT ONLY FOR LOCAL / DOCKER DEVELOPMENT
