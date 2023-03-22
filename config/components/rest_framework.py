510778d329a941c43b8b70d2136cec47ce963984
Artem Samoilov <samoylovartem07@gmail.com>
2023-03-22 11:26:40 +0800

    * Removes BrowsableAPIRenderer if DEBUG is not True

--- config/components/rest_framework.py
+++ config/components/rest_framework.py
@@ -1,10 +1,8 @@
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'apps.pagination.CustomPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -16,7 +14,6 @@ REST_FRAMEWORK = {
         'apps.permissions.CustomDjangoModelPermissions',
         # 'rest_framework_api_key.permissions.HasAPIKey',
         'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.DjangoModelPermissions',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
@@ -26,3 +23,8 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'rollbar.contrib.django_rest_framework.post_exception_handler',
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
+
+if DEBUG:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append(
+        'rest_framework.renderers.BrowsableAPIRenderer'
+    )
