a5fb043180bfc679b8a4cd7f30e32ce2bdfdbd7a
Artem Samoilov <samoylovartem07@gmail.com>
2023-05-22 15:16:25 +0800

    * Fixes some settings

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -10,9 +10,21 @@ dotenv_file = BASE_DIR / ('.env.dev' if DEBUG else '.env.prod')
 if os.path.isfile(dotenv_file):
     load_dotenv(dotenv_file)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(', ')
-
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(', ')
+ALLOWED_HOSTS = (
+    os.environ.get('ALLOWED_HOSTS', '').split(', ')
+    if os.environ.get('ALLOWED_HOSTS')
+    else []
+)
+CSRF_TRUSTED_ORIGINS = (
+    os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(', ')
+    if os.environ.get('CSRF_TRUSTED_ORIGINS')
+    else []
+)
+CORS_ALLOWED_ORIGINS = (
+    os.environ.get('CORS_ALLOWED_ORIGINS', '').split(', ')
+    if os.environ.get('CORS_ALLOWED_ORIGINS')
+    else []
+)
 
 include(
     'components/rest_framework.py',
@@ -141,8 +153,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(', ')
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
