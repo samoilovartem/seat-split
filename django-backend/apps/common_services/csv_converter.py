49cedcb76fa496daca18a789bfe786e9a4b5f2a7
Aaron Nicholas Gumapac <aarn.gmpc@gmail.com>
2023-06-13 14:11:34 +0800

    feat: methods for normalizing csv requests

---
+++ django-backend/apps/common_services/csv_converter.py
@@ -0,0 +1,57 @@
+from io import BytesIO, StringIO
+import django
+import pandas as pd
+from ast import literal_eval
+
+
+def csv_to_dict(data: str) -> list[dict[str, str]]:
+    """
+    Convert a list of csv strings to a list of dictionaries.
+
+    Args:
+        data: list of csv strings
+
+    Returns:
+        list of dictionaries
+    """
+
+    # convert csv string to dataframe
+    df = pd.read_csv(StringIO(data))
+    df.fillna("NA", inplace=True)
+
+    # convert dataframe to list of dictionaries
+    return df.to_dict("records")
+
+
+def dict_to_csv(data: list[dict[str, str]]) -> list[str]:
+    """
+    Convert a list of dictionaries to a bytes object.
+
+    Args:
+        data: list of dictionaries
+
+    Returns:
+        A bytes object representing the csv file.
+    """
+    csv = pd.DataFrame(data).to_csv(index=False)
+
+    # convert to bytes
+    return csv.encode("utf-8")
+
+
+def csv_to_file(data: list[str], file_name: str) -> StringIO:
+    """
+    Convert a list of csv strings to a StringIO object
+
+    Args:
+        data: list of csv strings
+        file_path: path to csv file
+    """
+
+    # convert to django file
+    data = StringIO("\n".join(data))
+    # cast to django file
+    data = django.core.files.base.ContentFile(data)
+    data.name = file_name
+
+    return data
---
+++ django-backend/apps/common_services/csv_normalizer.py
@@ -0,0 +1,102 @@
+from io import BytesIO
+from django.db.models.fields import NOT_PROVIDED
+from loguru import logger
+from apps.common_services.utils import get_model_fields, get_skippable_fields
+from apps.common_services.csv_converter import (
+    csv_to_dict,
+    dict_to_csv,
+)
+from django.apps import apps
+
+
+def apply_request_fields(
+    request, request_fields, app_name, model_name, exclude_fields=[]
+):
+    """
+    Applies all request fields to the csv file.
+
+    Args:
+        request: request object
+        request_fields: list of fields from the request
+        app_name: app name
+        model_name: model name
+
+    Returns:
+        A new request object with the updated data.
+    """
+
+    # get the csv file
+
+    csv_file = request.FILES.get("file").read().decode("utf-8")
+
+    # convert the csv file to a dictionary
+    csv_dictionary = csv_to_dict(csv_file)
+    logger.warning(f"csv_dictionary: {csv_dictionary}")
+
+    model_fields = get_model_fields(app_name, model_name, exclude_fields=exclude_fields)
+    request_fields = [
+        {key: value} for key, value in request.data.items() if key in model_fields
+    ]
+
+    for row in csv_dictionary:
+        for dictionaries in request_fields:
+            for key, value in dictionaries.items():
+                row[key] = value
+
+    # convert the csv file data back to a csv file
+    csv_file = dict_to_csv(csv_dictionary)
+
+    new_request = request
+    new_request.FILES.get("file").file = BytesIO(csv_file)
+
+    return new_request
+
+
+def normalize_csv_request(request, app_name, model_name, exclude_fields=[]):
+    """
+    This function is used to normalize the request data from the frontend.
+
+    If the csv file in the request does not have all fields within the Accounts model,
+    then the function will add the missing fields to the csv file.
+    """
+
+    # get the csv file
+    csv_file = str(request.FILES.get("file").read().decode("utf-8"))
+
+    # convert the csv file to a dictionary
+    csv_dict = csv_to_dict(csv_file)
+
+    # get the csv file headers
+    csv_file_headers = set(csv_dict[0].keys())
+    model_fields = get_model_fields(app_name, model_name, exclude_fields=exclude_fields)
+
+    # get the missing fields
+    missing_fields = [field for field in model_fields if field not in csv_file_headers]
+
+    # get default values
+    default_values = {
+        field.name: field.default
+        for field in apps.get_model(app_name, model_name)._meta.fields
+        if field.name in missing_fields
+    }
+
+    # add the missing fields to the csv file
+    for row in csv_dict:
+        for field in missing_fields:
+            row[field] = default_values[field]
+
+    for row in csv_dict:
+        for key, value in row.items():
+            if key in default_values:
+                row[key] = value
+            elif not value:
+                row[key] = "NA"
+
+    # convert the csv file data back to a csv file
+    csv_file = dict_to_csv(csv_dict)
+    logger.warning(f"csv_file: {csv_file}")
+
+    new_request = request
+    new_request.FILES["file"].file = BytesIO(csv_file)
+
+    return new_request
