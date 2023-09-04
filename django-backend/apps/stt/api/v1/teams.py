c8c4ee494476df7204717b1babb6fea546102299
samoylovartem <samoylovartem07@gmail.com>
2023-09-04 15:58:58 +0700

    feat: removes Teams endpoints from public view. Now it's available only with API KEY

--- django-backend/apps/stt/api/v1/teams.py
+++ django-backend/apps/stt/api/v1/teams.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_api_key.permissions import HasAPIKey
 
 from apps.stt.api.v1.serializers import TeamSerializer
 from apps.stt.models import Team
@@ -11,7 +11,7 @@ class TeamViewSet(ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     filterset_fields = ['league', 'city', 'name']
-    permission_classes = [AllowAny]
+    permission_classes = [HasAPIKey]
 
     my_tags = ['all teams']
 
