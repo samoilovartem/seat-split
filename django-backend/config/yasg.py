c819dbe4025855540bbec24341c6bc89455c7b28
samoylovartem <samoylovartem07@gmail.com>
2023-09-04 15:55:00 +0700

    feat: updates drf_yasg settings to allow authenticated user to see all available endpoints even if he doesn't have direct access to them

--- django-backend/config/yasg.py
+++ django-backend/config/yasg.py
@@ -2,7 +2,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import IsAuthenticated
 
-from django.urls import re_path
+from django.urls import path
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -12,23 +12,23 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email='asamoilov@lewanddowski.com'),
         license=openapi.License(name='BSD License'),
     ),
-    public=False,
+    public=True,
     permission_classes=[IsAuthenticated],
 )
 
 urlpatterns = [
-    re_path(
-        r'api/v1/swagger(?P<format>\.json|\.yaml)',
+    path(
+        'api/v1/swagger<format>/',
         schema_view.without_ui(cache_timeout=0),
         name='schema-json',
     ),
-    re_path(
-        r'api/v1/swagger/',
+    path(
+        'api/v1/swagger/',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui',
     ),
-    re_path(
-        r'api/v1/redoc/',
+    path(
+        'api/v1/redoc/',
         schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc',
     ),
