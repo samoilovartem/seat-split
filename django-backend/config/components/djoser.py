95bc43da2ea3be2b23b5c56e6fdad0ee48ab28d0
samoylovartem <samoylovartem07@gmail.com>
2023-09-04 15:56:04 +0700

    chore: updates permissions and serializers settings in djoser

--- django-backend/config/components/djoser.py
+++ django-backend/config/components/djoser.py
@@ -1,27 +1,23 @@
 DJOSER = {
     'HIDE_USERS': False,
     'PERMISSIONS': {
-        'activation': ['rest_framework.permissions.IsAuthenticated'],
+        'activation': ['rest_framework.permissions.AllowAny'],
         'password_reset': ['rest_framework.permissions.IsAuthenticated'],
         'password_reset_confirm': ['rest_framework.permissions.IsAuthenticated'],
         'set_password': ['rest_framework.permissions.IsAuthenticated'],
         'username_reset': ['rest_framework.permissions.IsAuthenticated'],
         'username_reset_confirm': ['rest_framework.permissions.IsAuthenticated'],
         'set_username': ['rest_framework.permissions.IsAuthenticated'],
-        'user_create': [
-            'apps.common_services.permissions.CustomDjangoModelPermissions'
-        ],
-        'user_delete': [
-            'apps.common_services.permissions.CustomDjangoModelPermissions'
-        ],
-        'user': ['rest_framework.permissions.IsAuthenticated'],
-        'user_list': ['apps.common_services.permissions.CustomDjangoModelPermissions'],
+        'user_create': ['apps.common_services.permissions.IsAdmin'],
+        'user_delete': ['apps.common_services.permissions.IsAdmin'],
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user_list': ['apps.common_services.permissions.IsAdmin'],
         'token_create': ['rest_framework.permissions.IsAuthenticated'],
         'token_destroy': ['rest_framework.permissions.IsAuthenticated'],
     },
     'SERIALIZERS': {
-        'current_user': 'apps.users.api.v1.serializers.UserDetailSerializer',
-        'user': 'djoser.serializers.UserSerializer',
-        'user_create': 'djoser.serializers.UserCreateSerializer',
+        'current_user': 'apps.users.api.v1.serializers.UserSerializer',
+        'user': 'apps.users.api.v1.serializers.UserSerializer',
+        # 'user_create': 'apps.users.api.v1.serializers.UserCreateSerializer',
     },
 }
