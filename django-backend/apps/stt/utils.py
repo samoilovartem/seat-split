08fbba1fb83bdf2b6975372c2f1dba60f592b503
samoylovartem <samoylovartem07@gmail.com>
2023-09-09 10:20:04 +0700

    feat: adds a new function send_email_confirmation for sending confirmation email using SMTP2GO settings and standard django email backend

--- django-backend/apps/stt/utils.py
+++ django-backend/apps/stt/utils.py
@@ -1,10 +1,14 @@
 import requests
+from rest_framework.request import Request
 
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
+from apps.users.models import User
 from config.components.smtp_and_email import (
     SMTP2GO_API_BASE_URL,
     SMTP2GO_API_KEY,
@@ -16,6 +20,7 @@ from config.components.smtp_and_email import (
 def send_email_confirmation_with_api(
     to: list, confirm_url: str, sender: str = SMTP2GO_FROM_EMAIL
 ):
+    """Sends email confirmation to user using SMTP2GO API."""
     url = f'{SMTP2GO_API_BASE_URL}/email/send'
     data = {
         'api_key': SMTP2GO_API_KEY,
@@ -32,7 +37,30 @@ def send_email_confirmation_with_api(
         return
 
 
-def get_confirmation_link(request, user):
+def send_email_confirmation(request: Request, user: User):
+    """Sends email confirmation to user using standard Django email backend."""
+    mail_subject = 'Activate your account in Ticket Hive'
+    message = render_to_string(
+        'emails/account_verification.html',
+        {'email': user.email, 'link': get_confirmation_link(request, user)},
+    )
+    to_email = user.email
+    email = EmailMessage(
+        subject=mail_subject,
+        body=message,
+        to=[to_email],
+        from_email='Ticket Hive <asamoilov@lewanddowski.com>',
+    )
+    email.content_subtype = 'html'
+    try:
+        email.send()
+    except Exception as e:
+        print(e)  # TODO: replace with logger
+        return
+
+
+def get_confirmation_link(request: Request, user: User):
+    """Returns link for email confirmation."""
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     domain = get_current_site(request).domain
