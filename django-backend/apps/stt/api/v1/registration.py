e4fc55786a8f4c67dba350742c4467dfbb64a17f
samoylovartem <samoylovartem07@gmail.com>
2023-09-09 16:22:43 +0700

    feat: adds a new function to send an email notifying a user about email being confirmed

--- django-backend/apps/stt/api/v1/registration.py
+++ django-backend/apps/stt/api/v1/registration.py
@@ -11,7 +11,7 @@ from django.utils.http import urlsafe_base64_decode
 
 from apps.stt.api.v1.serializers import RegisterSerializer
 from apps.stt.models import TicketHolder, User
-from apps.stt.utils import send_email_confirmation
+from apps.stt.utils import send_email_confirmation, send_email_confirmed
 
 
 class RegisterView(APIView):
@@ -26,6 +26,7 @@ class RegisterView(APIView):
             try:
                 password_validation.validate_password(password)
             except Exception as e:
+                # ToDo: add logger
                 return Response(
                     {'password': list(e.messages)}, status=status.HTTP_400_BAD_REQUEST
                 )
@@ -67,6 +68,8 @@ class VerifyView(APIView):
             if default_token_generator.check_token(user, self.kwargs['token']):
                 ticket_holder.is_verified = True
                 ticket_holder.save()
+
+                send_email_confirmed(request, user)  # ToDO move this function to Celery
 
                 return Response(status=status.HTTP_200_OK)
             else:
--- django-backend/apps/stt/utils.py
+++ django-backend/apps/stt/utils.py
@@ -2,7 +2,6 @@ import requests
 from rest_framework.request import Request
 
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
@@ -10,6 +9,9 @@ from django.utils.http import urlsafe_base64_encode
 
 from apps.users.models import User
 from config.components.smtp_and_email import (
+    EMAIL_CONTENT_TYPE,
+    EMAIL_FRONTEND_BASE_URL,
+    EMAIL_PROJECT_NAME,
     SMTP2GO_API_BASE_URL,
     SMTP2GO_API_KEY,
     SMTP2GO_EMAIL_CONFIRMATION_TEMPLATE_ID,
@@ -39,19 +41,23 @@ def send_email_confirmation_with_api(
 
 def send_email_confirmation(request: Request, user: User):
     """Sends email confirmation to user using standard Django email backend."""
-    mail_subject = 'Activate your account in Ticket Hive'
+    mail_subject = f'Activate your account in {EMAIL_PROJECT_NAME}'
     message = render_to_string(
         'emails/account_verification.html',
-        {'email': user.email, 'link': get_confirmation_link(request, user)},
+        {
+            'email': user.email,
+            'link': get_confirmation_link(request, user),
+            'project_name': EMAIL_PROJECT_NAME,
+        },
     )
     to_email = user.email
     email = EmailMessage(
         subject=mail_subject,
         body=message,
         to=[to_email],
-        from_email='Ticket Hive <asamoilov@lewanddowski.com>',
+        from_email=SMTP2GO_FROM_EMAIL,
     )
-    email.content_subtype = 'html'
+    email.content_subtype = EMAIL_CONTENT_TYPE
     try:
         email.send()
     except Exception as e:
@@ -59,10 +65,35 @@ def send_email_confirmation(request: Request, user: User):
         return
 
 
+def send_email_confirmed(request: Request, user: User):
+    """Sends email notifying that email is confirmed to user using standard Django email backend."""
+    mail_subject = f'Your account in {EMAIL_PROJECT_NAME} is confirmed'
+    message = render_to_string(
+        'emails/account_verified.html',
+        {
+            'email': user.email,
+            'link': f'{"https" if request.is_secure() else "http"}://{EMAIL_FRONTEND_BASE_URL}/',
+            'project_name': EMAIL_PROJECT_NAME,
+        },
+    )
+    to_email = user.email
+    email = EmailMessage(
+        subject=mail_subject,
+        body=message,
+        to=[to_email],
+        from_email=SMTP2GO_FROM_EMAIL,
+    )
+    email.content_subtype = EMAIL_CONTENT_TYPE
+    try:
+        email.send()
+    except Exception as e:
+        print(e)
+        return
+
+
 def get_confirmation_link(request: Request, user: User):
     """Returns link for email confirmation."""
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
-    domain = get_current_site(request).domain
-    confirmation_link = f'{"https" if request.is_secure() else "http"}://{domain}/confirm-email/{uid}/{token}/'
+    confirmation_link = f'{"https" if request.is_secure() else "http"}://{EMAIL_FRONTEND_BASE_URL}/api/confirm-email/{uid}/{token}/'
     return confirmation_link
