30e25e7acab76433d13ef861f426f5cc35ec82f6
samoylovartem <samoylovartem07@gmail.com>
2023-09-09 17:10:11 +0700

    chore: adds CustomTokenObtainPairView for jwt generating

--- django-backend/apps/urls.py
+++ django-backend/apps/urls.py
@@ -1,9 +1,10 @@
 from djoser.views import TokenDestroyView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from django.urls import include, path
 
 from apps.routers import main_router
-from apps.stt.api.v1.login import CustomTokenCreateView
+from apps.stt.api.v1.login import CustomTokenCreateView, CustomTokenObtainPairView
 from apps.stt.api.v1.registration import RegisterView, VerifyView
 
 urlpatterns = [
@@ -13,7 +14,13 @@ urlpatterns = [
         'api/v1/token-auth/token/login', CustomTokenCreateView.as_view(), name='login'
     ),
     path('api/v1/token-auth/token/logout', TokenDestroyView.as_view(), name='logout'),
-    path('api/v1/', include('djoser.urls.jwt')),
+    path(
+        'api/v1/auth/jwt/create/',
+        CustomTokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path('api/v1/auth/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/', include('apps.stt.api.v1.urls')),
     path('api/v1/', include(main_router.urls)),
     path('api/v1/register/', RegisterView.as_view(), name='register'),
