c6a5b624bef048a549e4edcfcc9277ba897814f3
samoylovartem <samoylovartem07@gmail.com>
2023-09-13 09:47:50 +0700

    feat: adds slack notification about deployment to Heroku
    * refactors urls

--- django-backend/apps/stt/api/v1/urls.py
+++ django-backend/apps/stt/api/v1/urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
+from djoser.views import TokenDestroyView
+
+from django.urls import include, path
 
 from apps.email_domains.api.v1.email_domains import (
     generate_random_data_with_provided_domain_or_state,
 )
+from apps.routers import main_router
+from apps.stt.api.v1.login import CustomTokenCreateView
+from apps.stt.api.v1.registration import RegisterView, VerifyView
 
 urlpatterns = [
     path(
         'api/v1/email-domains/generate_random_data_with_provided_domain_or_state/',
         generate_random_data_with_provided_domain_or_state,
     ),
+    path('token-auth/token/login', CustomTokenCreateView.as_view(), name='login'),
+    path('token-auth/token/logout', TokenDestroyView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('verify/<str:uidb64>/<str:token>/', VerifyView.as_view(), name='verify'),
+    path('', include(main_router.urls)),
 ]
