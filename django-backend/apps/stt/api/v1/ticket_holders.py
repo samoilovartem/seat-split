dc08919bfd894a986949c505c287415e71a7a3d5
samoylovartem <samoylovartem07@gmail.com>
2023-09-14 20:50:11 +0700

    feat: adds an endpoint to create a record in TicketHolderTeam table

--- django-backend/apps/stt/api/v1/ticket_holders.py
+++ django-backend/apps/stt/api/v1/ticket_holders.py
@@ -1,3 +1,6 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.mixins import (
     DestroyModelMixin,
     ListModelMixin,
@@ -5,9 +8,13 @@ from rest_framework.mixins import (
     UpdateModelMixin,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from apps.stt.api.v1.serializers import TicketHolderSerializer
+from apps.stt.api.v1.serializers import (
+    TicketHolderSerializer,
+    TicketHolderTeamSerializer,
+)
 from apps.stt.models import TicketHolder
 
 
@@ -24,3 +31,14 @@ class TicketHolderViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @swagger_auto_schema(request_body=TicketHolderTeamSerializer)
+    @action(detail=True, methods=['POST'])
+    def add_ticket_holder_team(self, request, pk=None):
+        ticket_holder = self.get_object()
+        team_serializer = TicketHolderTeamSerializer(data=request.data)
+
+        if team_serializer.is_valid():
+            team_serializer.save(ticket_holder=ticket_holder)
+            return Response(team_serializer.data)
+        return Response(team_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
