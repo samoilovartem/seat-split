8c515356848bf8d8f0fffe433a34a0c1fe96cbc3
samoylovartem <samoylovartem07@gmail.com>
2023-09-28 10:56:35 +0700

    fix: fixes root value issue as rollbar expect BASE_DIR as string

--- django-backend/config/components/rollbar.py
+++ django-backend/config/components/rollbar.py
@@ -6,5 +6,5 @@ ROLLBAR = {
     'access_token': os.environ.get('ROLLBAR_ACCESS_TOKEN'),
     'environment': 'development' if DEBUG else 'production',
     'code_version': '1.0',
-    'root': BASE_DIR,
+    'root': str(BASE_DIR),
 }
