8cb55ff11bb040b423387e2e3b5e54885e18e44c
samoylovartem <samoylovartem07@gmail.com>
2023-10-11 16:14:39 +0700

    feat: creates custom filters for Purchases views

--- django-backend/config/components/django_filters.py
+++ django-backend/config/components/django_filters.py
@@ -1,3 +1,3 @@
-BOOL_LOOKUPS = ['exact']
+BOOL_OR_EXACT_LOOKUPS = ['exact']
 DATE_AND_ID_LOOKUPS = ['exact', 'range', 'in', 'gte', 'lte']
 CHAR_LOOKUPS = ['icontains', 'iexact', 'exact', 'startswith', 'contains', 'endswith']
