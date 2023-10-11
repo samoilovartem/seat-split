1036750216cbcbf89a8b98c794a333be625221c6
samoylovartem <samoylovartem07@gmail.com>
2023-10-11 16:13:38 +0700

    feat: adds full CRUD for Purchase model

---
+++ django-backend/apps/stt/api/v1/purchases.py
@@ -0,0 +1,22 @@
+from rest_framework.viewsets import ModelViewSet
+
+from apps.common_services.permissions import IsTicketHolder
+from apps.stt.api.v1.serializers import PurchaseSerializer
+from apps.stt.filters import PurchaseFilterSet
+from apps.stt.models import Purchase
+
+
+class PurchaseViewSet(ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseSerializer
+    filterset_class = PurchaseFilterSet
+    permission_classes = (IsTicketHolder,)
+    my_tags = ['purchases']
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff or user.is_superuser:
+            return Purchase.objects.all()
+
+        return Purchase.objects.filter(ticket__ticket_holder=user.ticket_holder_user)
