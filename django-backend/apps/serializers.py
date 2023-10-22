1e10c8bdbbc6c3e68be3b1e080593147a4a4bc5e
samoylovartem <samoylovartem07@gmail.com>
2023-10-22 19:28:13 +0700

    feat: adds AvailableSeatsCalculator class that holds all logic to calculate available seats for all events related to ticket_holder
    * moves "available_seats" action endpoint to events views

--- django-backend/apps/serializers.py
+++ django-backend/apps/serializers.py
@@ -16,28 +16,6 @@ class ShowAllSeatsMixin:
         return representation
 
 
-# class ShowAllSeatsMixin:
-#     def to_representation(self, instance):
-#         representation = super().to_representation(instance)
-#         if '-' in representation['seat']:
-#             first_seat, last_seat = representation['seat'].split('-')
-#             all_seats = set(str(i) for i in range(int(first_seat), int(last_seat) + 1))
-#
-#             # Get tickets belonging to the ticket holder
-#             tickets_for_holder = Ticket.objects.filter(ticket_holder=instance.ticket_holder)
-#
-#             # Gather all the seats that are already used
-#             used_seats = set(ticket.seat for ticket in tickets_for_holder)
-#
-#             # Remove used seats from the list of all seats
-#             available_seats = all_seats - used_seats
-#
-#             representation['seat'] = list(available_seats)
-#         else:
-#             representation['seat'] = [representation['seat']]
-#         return representation
-
-
 class ConvertNoneToStringSerializerMixin:
     def get_none_to_str_fields(self):
         meta = getattr(self, 'Meta', None)
