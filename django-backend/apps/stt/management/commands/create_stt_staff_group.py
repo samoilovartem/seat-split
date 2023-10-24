4b4c0e29bfe7bded3afc94e453cb4268cc7d78b6
samoylovartem <samoylovartem07@gmail.com>
2023-10-24 14:07:48 +0700

    feat: adds a special CLI command to create Staff group with its permissions

---
+++ django-backend/apps/stt/management/commands/create_stt_staff_group.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+
+from config.components.business_related import (
+    STT_STAFF_GROUP_NAME,
+    STT_STAFF_GROUP_PERMISSIONS,
+)
+
+
+class Command(BaseCommand):
+    help = f"Create the {STT_STAFF_GROUP_NAME} group if it doesn't exist."
+
+    def handle(self, *args, **options):
+        group_name = STT_STAFF_GROUP_NAME
+
+        if not Group.objects.filter(name=group_name).exists():
+            group = Group(name=group_name)
+            group.save()
+
+            for perm_codename in STT_STAFF_GROUP_PERMISSIONS:
+                try:
+                    permission = Permission.objects.get(codename=perm_codename)
+                    group.permissions.add(permission)
+                except Permission.DoesNotExist:
+                    self.stderr.write(
+                        self.style.ERROR(f'Permission {perm_codename} not found.')
+                    )
+
+            self.stdout.write(
+                self.style.SUCCESS(f'Group {group_name} created successfully!')
+            )
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Group {group_name} already exists.'))
--- django-backend/entrypoint-prod.sh
+++ django-backend/entrypoint-prod.sh
@@ -6,6 +6,9 @@ python manage.py collectstatic --noinput
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Creating STT staff group..."
+python manage.py create_stt_staff_group --noinput
+
 echo "Starting server..."
 gunicorn --timeout "$GUNICORN_TIMEOUT" --bind 0.0.0.0:"$PORT" config.wsgi:application
 
