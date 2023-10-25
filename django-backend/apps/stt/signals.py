c0ee5f7c0debabd6d6a8e10127792e369432efdb
samoylovartem <samoylovartem07@gmail.com>
2023-10-25 17:24:27 +0700

    feat: adds a functionality that will allow ticket holder to delist his/her ticket/s and notify our staff, so they manually delist it/them via our platforms

--- django-backend/apps/stt/signals.py
+++ django-backend/apps/stt/signals.py
@@ -1,8 +1,12 @@
+from loguru import logger
+
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
-from apps.stt.models import TicketHolderTeam
-from apps.stt.tasks import send_ticket_holder_team_confirmed
+from apps.stt.models import Ticket, TicketHolderTeam
+from apps.stt.tasks import send_ticket_holder_team_confirmed, send_to_slack
+from apps.stt.utils import create_slack_message
+from config.components.global_settings import DEBUG
 
 
 @receiver(pre_save, sender=TicketHolderTeam)
@@ -25,3 +29,22 @@ def send_confirmation_email(sender, instance, **kwargs):
         #     user_email=instance.ticket_holder.user.email,
         #     team_name=instance.team.name
         # )
+
+
+@receiver(post_save, sender=Ticket)
+def ticket_post_save(sender, instance, **kwargs):
+    if kwargs.get('created', False) or len(instance.history.all()) < 2:
+        return
+
+    previous_status = instance.history.all()[1].listing_status
+    if previous_status == 'Cancelled' or instance.listing_status != 'Cancelled':
+        return
+
+    message = create_slack_message(instance)
+
+    if DEBUG:
+        logger.info('DEBUG MODE: Slack notification has not been sent.')
+        return
+
+    send_to_slack(message)
+    # send_to_slack.delay(message)
--- django-backend/apps/stt/utils.py
+++ django-backend/apps/stt/utils.py
@@ -6,7 +6,9 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
+from apps.stt.models import Ticket
 from apps.users.models import User
+from config.components.slack_integration import STT_NOTIFICATIONS_CHANNEL_TICKET_URL
 from config.components.smtp_and_email import (
     EMAIL_FRONTEND_BASE_URL,
     SMTP2GO_API_BASE_URL,
@@ -50,3 +52,37 @@ def get_confirmation_link(user_id: UUID):
     )
 
     return confirmation_link
+
+
+def create_slack_message(instance: Ticket) -> dict[str, str]:
+    """Function to create the Slack message payload."""
+    return {
+        'text': f'Ticket {instance.id} for {instance.ticket_holder} - {instance.event} has been cancelled!',
+        'blocks': [
+            {
+                'type': 'header',
+                'text': {'type': 'plain_text', 'text': 'Ticket Delisting Alert'},
+            },
+            {
+                'type': 'section',
+                'fields': [
+                    {
+                        'type': 'mrkdwn',
+                        'text': f'*TICKET HOLDER:*\n{instance.ticket_holder}',
+                    },
+                    {'type': 'mrkdwn', 'text': f'*ID:*\n`{instance.ticket_holder.id}`'},
+                    {'type': 'mrkdwn', 'text': f'*EVENT:*\n{instance.event}'},
+                    {'type': 'mrkdwn', 'text': f'*TICKET ID:*\n`{instance.id}`'},
+                ],
+            },
+            {'type': 'divider'},
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f'The above ticket has been requested for delisting. Please take necessary '
+                    f'actions. \n\n<{STT_NOTIFICATIONS_CHANNEL_TICKET_URL}/{instance.id}/change/|View Ticket>',
+                },
+            },
+        ],
+    }
