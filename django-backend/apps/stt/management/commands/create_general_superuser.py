03b3865d593b2fd3264fad76ae36b22c19d8d2ba
samoylovartem <samoylovartem07@gmail.com>
2023-11-01 12:05:24 +0700

    feat: adds a command to create a general superuser

---
+++ django-backend/apps/stt/management/commands/create_general_superuser.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+
+from apps.users.models import User
+from config.settings import (
+    GENERAL_SUPERUSER_EMAIL,
+    GENERAL_SUPERUSER_PASSWORD,
+    GENERAL_SUPERUSER_USERNAME,
+)
+
+
+class Command(BaseCommand):
+    help = 'Create a general superuser if it does not exist'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(is_superuser=True).exists():
+            try:
+                User.objects.create_superuser(
+                    GENERAL_SUPERUSER_USERNAME,
+                    GENERAL_SUPERUSER_EMAIL,
+                    GENERAL_SUPERUSER_PASSWORD,
+                )
+                self.stdout.write(
+                    self.style.SUCCESS('Successfully created a new superuser')
+                )
+            except IntegrityError:
+                self.stdout.write(
+                    self.style.WARNING('A superuser with that username already exists.')
+                )
+        else:
+            self.stdout.write(self.style.SUCCESS('A superuser already exists.'))
