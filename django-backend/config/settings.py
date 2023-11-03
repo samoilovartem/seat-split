5661f7f859eb38f0ac72aeaa5b1de7f0907275aa
samoylovartem <samoylovartem07@gmail.com>
2023-11-03 18:47:07 +0700

    feat: adds django-request-logging to test if it will work with railway

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -36,7 +36,7 @@ include(
     'components/query_count.py',
     'components/django_filters.py',
     'components/swagger.py',
-    'components/logger.py',
+    # 'components/logger.py',
     'components/smtp_and_email.py',
     'components/celery.py',
     'components/business_related.py',
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     'config.middlewares.CustomRollbarNotifierMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
     'querycount.middleware.QueryCountMiddleware',
+    'request_logging.middleware.LoggingMiddleware',
 ]
 
 TEMPLATES = [
@@ -151,6 +152,45 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
-
 GENERAL_SUPERUSER_EMAIL = os.environ.get('GENERAL_SUPERUSER_EMAIL', 'admin@example.com')
 GENERAL_SUPERUSER_PASSWORD = os.environ.get('GENERAL_SUPERUSER_PASSWORD', 'qwerty123@')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            'propagate': False,
+        },
+        'website': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
