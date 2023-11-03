5d4c8d3b5d27537f5b6b4b2e75c2095b8af0c80f
samoylovartem <samoylovartem07@gmail.com>
2023-11-03 15:19:41 +0700

    fix: fixes a bug IntegrityError: duplicate key while creating a user in admin dashboard

---
+++ django-backend/apps/users/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from apps.users.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '').lower()
+        if not email:
+            raise forms.ValidationError("The email field cannot be blank.")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f"The email {email} is already in use.")
+        return email
