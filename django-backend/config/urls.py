e3d06c862bb0969c11877e498ce98cf179f5ae92
samoylovartem <samoylovartem07@gmail.com>
2023-11-14 15:56:53 +0700

    feat: adds an option to upload avatar for ticket holder
    * installs Pillow as dependency
    
    * adds migration file

--- django-backend/config/urls.py
+++ django-backend/config/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from config.settings import MEDIA_ROOT, MEDIA_URL
 from config.yasg import urlpatterns as swagger_urls
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('', include('apps.urls')),
 ]
 
-urlpatterns += swagger_urls
+urlpatterns += swagger_urls + static(MEDIA_URL, document_root=MEDIA_ROOT)
