7001df2e782ad83d99e111de124452f5c33f10ad
samoylovartem <samoylovartem07@gmail.com>
2023-11-16 18:36:04 +0700

    fix: adds small fixes for sending contact us notification logic

--- django-backend/apps/support/tasks.py
+++ django-backend/apps/support/tasks.py
@@ -4,15 +4,24 @@ from loguru import logger
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
-from config.components.smtp_and_email import EMAIL_CONTENT_TYPE, SMTP2GO_SUPPORT_EMAIL
+from config.components.smtp_and_email import (
+    EMAIL_CONTENT_TYPE,
+    EMAIL_PROJECT_NAME,
+    SMTP2GO_FROM_EMAIL,
+    SMTP2GO_SUPPORT_EMAIL,
+)
 
 
 @shared_task
-def send_contact_us_notification(user_email: str, subject: str, message: str):
+def send_contact_us_notification(
+    user_email: str, subject: str, message: str, first_name: str, last_name: str
+):
     """Sends notification to support staff when a contact form is submitted."""
     support_email_body = render_to_string(
         'emails/contact_notification.html',
         {
+            'project_name': EMAIL_PROJECT_NAME,
+            'user_full_name': f'{first_name} {last_name}',
             'user_email': user_email,
             'message': message,
             'subject': subject,
@@ -22,7 +31,7 @@ def send_contact_us_notification(user_email: str, subject: str, message: str):
         subject=subject,
         body=support_email_body,
         to=[SMTP2GO_SUPPORT_EMAIL],
-        from_email=user_email,
+        from_email=SMTP2GO_FROM_EMAIL,
     )
     email.content_subtype = EMAIL_CONTENT_TYPE
     try:
