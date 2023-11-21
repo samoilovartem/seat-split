6db8d2c2e549782c1d14d77ba4bf222a9af56e2a
samoylovartem <samoylovartem07@gmail.com>
2023-11-21 10:56:47 +0700

    feat: adds a rate limit for API

--- django-backend/config/components/rest_framework.py
+++ django-backend/config/components/rest_framework.py
@@ -23,6 +23,11 @@ REST_FRAMEWORK = {
     ],
     'EXCEPTION_HANDLER': 'rollbar.contrib.django_rest_framework.post_exception_handler',
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {'anon': '15/hour', 'user': '100/hour'},
 }
 
 if DEBUG:
