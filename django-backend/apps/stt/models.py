b0cf61d8e77c734b9adcdb74b197056259e50b01
samoylovartem <samoylovartem07@gmail.com>
2023-12-11 13:21:54 +0700

    chore(models): remove 'season' field from Ticket model
    The corresponding migration file has been created to reflect these changes in the database. This removal is a part of a larger model restructuring process. 'season' for Ticket model is now automatically populated from Event model

--- django-backend/apps/stt/models.py
+++ django-backend/apps/stt/models.py
@@ -56,7 +56,6 @@ class Ticket(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     ticket_holder = models.ForeignKey(TicketHolder, on_delete=models.CASCADE)
     event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='tickets')
-    season = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     seat = models.CharField(max_length=255)
     row = models.CharField(max_length=255)
