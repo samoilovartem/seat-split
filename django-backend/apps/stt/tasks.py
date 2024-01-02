7bef6c27a7a4d78be391d48a7d803d961f72dab5
samoylovartem <samoylovartem07@gmail.com>
2024-01-02 16:15:05 +0700

    feat(tasks): add periodic tasks for `clean_duplicate_history` and `clean_old_history` (django-simple-history) to avoid keeping millions of old and useless records

--- django-backend/apps/stt/tasks.py
+++ django-backend/apps/stt/tasks.py
@@ -9,6 +9,7 @@ from loguru import logger
 from slack_sdk.errors import SlackApiError
 
 from django.core.mail import EmailMessage
+from django.core.management import call_command
 from django.template.loader import render_to_string
 from django.utils.timezone import now
 
@@ -259,3 +260,27 @@ def fetch_and_send_issues_report():
         args=(slack_message, STT_WEEKLY_ISSUES_REPORT_CHANNEL_ID),
         countdown=CELERY_GENERAL_COUNTDOWN,
     )
+
+
+@shared_task
+def clean_duplicate_history(minutes=None, excluded_fields=None, use_base_manager=False):
+    """Removes duplicate historical records."""
+    command_options = {'auto': True}
+    if minutes:
+        command_options['minutes'] = minutes
+    if excluded_fields:
+        command_options['excluded_fields'] = excluded_fields
+    if use_base_manager:
+        command_options['base_manager'] = True
+
+    logger.info('Cleaning duplicate history...')
+    call_command('clean_duplicate_history', **command_options)
+    logger.info('Cleaning duplicate history finished.')
+
+
+@shared_task
+def clean_old_history(days=30):
+    """Removes historical records that have existed for a certain amount of time."""
+    logger.info('Cleaning old history...')
+    call_command('clean_old_history', auto=True, days=days)
+    logger.info('Cleaning old history finished.')
