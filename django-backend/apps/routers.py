2da42f6489e06d792842f8b55ec905d61d15d8a9
samoylovartem <samoylovartem07@gmail.com>
2024-01-06 18:25:01 +0700

    feat(api): add routers in a new app

--- django-backend/apps/routers.py
+++ django-backend/apps/routers.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
+from apps.in_app_notifications.api import in_app_notifications_api_router_v1
 from apps.stt.api import stt_api_router_v1
 from apps.users.api import users_api_router_v1
 
 main_router = routers.DefaultRouter()
 main_router.registry.extend(stt_api_router_v1.registry)
 main_router.registry.extend(users_api_router_v1.registry)
+main_router.registry.extend(in_app_notifications_api_router_v1.registry)
