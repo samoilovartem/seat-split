bac38eff5faec88ed5831603ad9811825a9c7cee
samoylovartem <samoylovartem07@gmail.com>
2024-01-07 11:12:16 +0700

    feat(api): add notifications endpoints and serializers via viewsets and action decorator

---
+++ django-backend/apps/in_app_notifications/api/serializers.py
@@ -0,0 +1,40 @@
+from notifications.models import Notification
+from rest_framework.serializers import ModelSerializer
+
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+
+User = get_user_model()
+
+
+class SimpleUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name']
+
+
+class ContentTypeSerializer(ModelSerializer):
+    class Meta:
+        model = ContentType
+        fields = ['app_label', 'model']
+
+
+class NotificationSerializer(ModelSerializer):
+    recipient = SimpleUserSerializer()
+    actor_content_type = ContentTypeSerializer()
+
+    class Meta:
+        model = Notification
+        fields = [
+            'id',
+            'recipient',
+            'actor_content_type',
+            'verb',
+            'level',
+            'description',
+            'unread',
+            'public',
+            'deleted',
+            'emailed',
+            'timestamp',
+        ]
