5c6c49536c98a444d4df1b6608801fe948f2ee3d
samoylovartem <samoylovartem07@gmail.com>
2024-01-09 10:59:50 +0700

    refactor(signals): pass `redis_celery_connection` as DI instead of calling it directly in `TicketCacheService` class

--- django-backend/apps/stt/signals.py
+++ django-backend/apps/stt/signals.py
@@ -2,8 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.stt.models import Ticket, TicketHolderTeam
-from apps.stt.services.ticket_handler import TicketPostSaveHandler
 from apps.stt.services.ticket_holder_team_handler import TicketHolderTeamPostSaveHandler
+from apps.stt.services.ticket_post_save_handler import TicketPostSaveHandler
+from config.components.redis import redis_celery_connection
 
 
 @receiver(post_save, sender=TicketHolderTeam)
@@ -22,5 +23,5 @@ def ticket_post_save(sender, instance, created, **kwargs):
     Send a Slack notification when a ticket is created or requested for delisting.
     Create Purchase record and send an email when ticket is sold.
     """
-    handler = TicketPostSaveHandler(instance, created)
+    handler = TicketPostSaveHandler(instance, created, redis_celery_connection)
     handler.handle()
