fbc118313743fe3d6918a3a67b6d1b19a13e63cf
samoylovartem <samoylovartem07@gmail.com>
2024-01-29 10:35:29 +0700

    test: push "bad" code to test `ruff`

--- django-backend/apps/stt/forms.py
+++ django-backend/apps/stt/forms.py
@@ -1,6 +1,8 @@
-from apps.stt.models import Ticket
 from django import forms
+from apps.stt.models import Ticket
 from django.core.exceptions import ValidationError
+
+
 
 
 class TicketAdminForm(forms.ModelForm):
