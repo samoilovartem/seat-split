4a9d55465284e223981a737c83e57fb1960bee05
samoylovartem <samoylovartem07@gmail.com>
2024-02-21 10:56:39 +0700

    refactor(utils): refactor `skybox_api_event_fetcher` to use a class structure

--- utils/skybox_api_event_fetcher.py
+++ utils/skybox_api_event_fetcher.py
@@ -1,50 +1,57 @@
 import json
 
 import requests
+from loguru import logger
+from requests import Response
 from settings import settings
 
-LEAGUE = 'MLS'
 
+class SportsEventsFetcher:
+    def __init__(self, league: str, start_year: int):
+        self.league = league
+        self.start_year = start_year
+        self.event_date_from, self.event_date_to = self._fetch_seasons_dates()
 
-def fetch_seasons_dates(league: str, start_year: int):
-    response = requests.get(
-        f'{settings.stt_endpoint}{settings.stt_seasons_prefix}',
-        headers={'Authorization': f'Token {settings.stt_auth_token}'},
-        params={'league': league, 'start_year': start_year},
-    )
+    def _fetch_seasons_dates(self):
+        response = requests.get(
+            url=f'{settings.stt_endpoint}{settings.stt_seasons_prefix}',
+            headers=settings.get_stt_authorization_header,
+            params={'league': self.league, 'start_year': self.start_year},
+        )
 
-    start_regular_season_date = response.json()[0]['start_regular_season_date']
-    end_regular_season_date = response.json()[0]['end_regular_season_date']
+        data = response.json().get('results')[0]
+        start_regular_season_date = data['start_regular_season_date']
+        end_regular_season_date = data['end_regular_season_date']
 
-    return start_regular_season_date, end_regular_season_date
+        return start_regular_season_date, end_regular_season_date
 
+    def _create_params(self):
+        return {
+            'limit': 4000,
+            'eventType': 'SPORT',
+            'excludeParking': 'true',
+            'category': self.league,
+            'eventDateFrom': self.event_date_from,
+            'eventDateTo': self.event_date_to,
+        }
 
-event_date_from, event_date_to = fetch_seasons_dates(LEAGUE, 2024)
+    def fetch_events(self):
+        response = requests.get(
+            url=settings.get_skybox_events_endpoint,
+            headers=settings.get_skybox_api_headers,
+            params=self._create_params(),
+        )
+        self._save_to_json_file(response)
+        return response.json()
 
-
-params = {
-    'limit': 4000,
-    'eventType': 'SPORT',
-    'excludeParking': 'true',
-    'category': LEAGUE,
-    'eventDateFrom': event_date_from,
-    'eventDateTo': event_date_to,
-}
-
-
-def fetch_events():
-    response = requests.get(
-        settings.get_skybox_events_endpoint, headers=settings.get_skybox_api_headers, params=params
-    )
-    save_to_json_file(response)
-    return response.json()
-
-
-def save_to_json_file(response):
-    with open('events.json', 'w') as f:
-        json.dump(response.json(), f, indent=4)
+    @staticmethod
+    def _save_to_json_file(response: Response):
+        with open('events.json', 'w') as f:
+            json.dump(response.json(), f, indent=4)
 
 
 if __name__ == '__main__':
-    # print(fetch_events())
-    print(fetch_seasons_dates('MLS', 2024))
+    league = 'MLS'
+    start_year = 2024
+    sports_events_fetcher = SportsEventsFetcher(league, start_year)
+    logger.info(json.dumps(sports_events_fetcher.fetch_events(), indent=4))
