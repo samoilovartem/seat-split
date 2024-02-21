02935a4ceb36db3d08956549cabdd7bf8e395bfa
samoylovartem <samoylovartem07@gmail.com>
2024-02-21 20:38:41 +0700

    feat(utils): refactor TiqAssist API and enhance functionality.
    A new class, `TiqAssistDataProcessor`, was introduced, facilitating the fetching and processing of STT teams and exporting them to CSV. Error handling has been improved throughout, with useful problem-specific logging messages.

--- utils/tiqassist_api.py
+++ utils/tiqassist_api.py
@@ -1,27 +1,68 @@
 import csv
 
 import requests
+from loguru import logger
+from rest_framework import status
+from settings import settings
 
-MAIN_URL = 'https://app.tiqassist.com/api/registration/options'
 
-response = requests.get(MAIN_URL)
+class TiqAssistDataProcessor:
+    def __init__(self):
+        self.stt_teams_fields = self.fetch_stt_teams_fields()
+        self.fields_to_exclude = ['id', 'created_at']
+        self.required_fields = [
+            field for field in self.stt_teams_fields if field not in self.fields_to_exclude
+        ]
 
-if response.status_code == 200:
-    data = response.json()
+    def fetch_stt_teams_fields(self):
+        response = requests.get(
+            url=settings.get_stt_teams_endpoint, headers=settings.get_stt_authorization_header
+        )
+        if response.status_code == status.HTTP_200_OK:
+            data = response.json()
+            if data['results']:
+                return list(data['results'][0].keys())
+        else:
+            logger.error(f'Failed to fetch fields from my endpoint: {response.status_code}')
+            return []
+        return []
 
-    teams = [team for team in data['teams'] if team['league'] != 'NCAAB']
+    def fetch_and_process_teams(self):
+        response = requests.get(settings.tiqassist_endpoint)
+        if response.status_code == status.HTTP_200_OK:
+            data = response.json()
+            teams = [team for team in data['teams'] if team['league'] == 'NCAAF']
 
-    csv_file = 'teams.csv'
+            for team in teams:
+                if 'id' in team:
+                    team['skybox_id'] = team.pop('id')
 
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
-        fieldnames = teams[0].keys()
-        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+            filtered_teams = self.filter_teams(teams)
+            self.export_to_csv(filtered_teams)
+        else:
+            logger.error(f'Failed to fetch data from tiqassist: {response.status_code}')
 
-        csv_writer.writeheader()
-
+    def filter_teams(self, teams):
+        filtered_teams = []
         for team in teams:
-            csv_writer.writerow(team)
+            filtered_team = {key: team[key] for key in self.stt_teams_fields if key in team}
+            filtered_teams.append(filtered_team)
+        return filtered_teams
 
-    print(f"CSV file '{csv_file}' created successfully.")
-else:
-    print(f'Failed to fetch data: {response.status_code}')
+    def export_to_csv(self, filtered_teams):
+        if filtered_teams:
+            csv_file = 'ncaaf_teams.csv'
+            with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+                fieldnames = filtered_teams[0].keys()
+                csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+                csv_writer.writeheader()
+                for team in filtered_teams:
+                    csv_writer.writerow(team)
+            logger.info(f"CSV file '{csv_file}' created successfully.")
+        else:
+            logger.info('No matching fields found between endpoints.')
+
+
+if __name__ == '__main__':
+    teams_data_processor = TiqAssistDataProcessor()
+    teams_data_processor.fetch_and_process_teams()
