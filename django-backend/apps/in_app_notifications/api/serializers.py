60dc8d22711d8852d3df2d30ebe656b66ccbbc46
samoylovartem <samoylovartem07@gmail.com>
2024-03-14 12:59:35 +0700

    feat(api): update `NotificationSerializer` with `CreateModelMixin`
    The `NotificationSerializer` class in the in_app_notifications application has been modified. New fields have been added to facilitate creating new notification instances, and a `create` method has been implemented to handle the creation process. This allows the `NotificationSerializer` to not only serialize notification data but also deal with notification creation.

--- django-backend/apps/in_app_notifications/api/serializers.py
+++ django-backend/apps/in_app_notifications/api/serializers.py
@@ -1,4 +1,5 @@
 from notifications.models import Notification
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from django.contrib.auth import get_user_model
@@ -20,8 +21,13 @@ class ContentTypeSerializer(ModelSerializer):
 
 
 class NotificationSerializer(ModelSerializer):
-    recipient = SimpleUserSerializer()
-    actor_content_type = ContentTypeSerializer()
+    recipient = SimpleUserSerializer(read_only=True)
+    actor_content_type = ContentTypeSerializer(read_only=True)
+
+    recipient_email = serializers.EmailField(write_only=True, required=True)
+    actor_object_id = serializers.UUIDField(write_only=True, required=True)
+    actor_content_type_app_label = serializers.CharField(write_only=True, required=True)
+    actor_content_type_model = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = Notification
@@ -29,6 +35,7 @@ class NotificationSerializer(ModelSerializer):
             'id',
             'recipient',
             'actor_content_type',
+            'actor_object_id',
             'verb',
             'level',
             'description',
@@ -37,4 +44,21 @@ class NotificationSerializer(ModelSerializer):
             'deleted',
             'emailed',
             'timestamp',
+            'recipient_email',
+            'actor_content_type_app_label',
+            'actor_content_type_model',
         ]
+
+    def create(self, validated_data):
+        recipient_email = validated_data.pop('recipient_email', None)
+        app_label = validated_data.pop('actor_content_type_app_label', None)
+        model = validated_data.pop('actor_content_type_model', None)
+
+        if recipient_email:
+            recipient = User.objects.get(email=recipient_email)
+            validated_data['recipient'] = recipient
+        if app_label and model:
+            actor_content_type = ContentType.objects.get(app_label=app_label, model=model)
+            validated_data['actor_content_type'] = actor_content_type
+
+        return super().create(validated_data)
