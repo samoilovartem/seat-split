4fd388fede40e589e70523f14ea2eecaad043b77
Artem Samoilov <samoylovartem07@gmail.com>
2025-05-31 13:00:47 +0700

    feat(settings): add staticfiles storage backend
    Introduce a new storage backend configuration for static files. This addition allows the application to handle static files using Django's StaticFilesStorage.

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -147,6 +147,9 @@ STORAGES = {
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+    },
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
